38551b17da0950f2d31a459d1b4878fa1826916c
siddhijagtap7 <siddhijagtap802@gmail.com>
2024-11-17 13:54:25 +0530

    update license and minor fixes

---
+++ README.md
@@ -0,0 +1,2 @@
+# AI Bill Splitter
+
--- main.py
+++ main.py
@@ -5,19 +5,41 @@ import core.model as m
 from core.dataframe import get_individual_prices
 import pandas as pd
 import pytesseract
+import os
 
-pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
+logo = Image.open("assets/logo.png")
+st.set_page_config(page_title="Bill Splitter", page_icon=logo)
 
-st.title("Bill Splitter")
+hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;} /* Hides the hamburger menu */
+    footer {visibility: hidden;}    /* Hides the footer */
+    </style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+if os.path.exists("/usr/bin/tesseract"):
+    pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
+else:
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+
+col1, col2 = st.columns([0.8, 4])
+
+with col1:
+    st.image(logo, width=90)  
+
+with col2:
+    st.title("Bill Splitter")
 
 n = st.text_input("Enter the number of people:")
-if n.isdigit():
+if n.isdigit() and int(n) > 0:
     n = int(n)
 else:
     st.error("Please enter a valid number of people.")
     st.stop()
 
-uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Upload the bill", type=["jpg", "jpeg", "png"])
 
 @st.cache_data
 def process_image(file):
@@ -30,9 +52,7 @@ def process_image(file):
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image', use_column_width=True)
-
     df_items, df_price = process_image(uploaded_file)
-
     rows = df_items.shape[0]
 
     if "selections" not in st.session_state:
@@ -40,7 +60,6 @@ if uploaded_file is not None:
 
     def update_checkbox_state(row, col, checked):
         st.session_state.selections[row][col] = int(checked)
-
     st.write("Select options from the grid and enter your input below:")
 
     for i in range(rows):
@@ -56,7 +75,7 @@ if uploaded_file is not None:
                     args=(i, j, not st.session_state.selections[i][j]),
                 )
 
-    if st.button("Calculate Individual Prices"):
+    if st.button("Split"):
         mat = st.session_state.selections
         amt = get_individual_prices(mat, df_items, df_price)
         split_mat = []
@@ -67,10 +86,7 @@ if uploaded_file is not None:
         
         st.write("Amount to be paid:")
         st.write(split_mat)
-
     # st.write("Items and Prices:")
     # st.write(df_items)
     # st.write("Price Breakdown with GST:")
     # st.write(df_price)
-
-
